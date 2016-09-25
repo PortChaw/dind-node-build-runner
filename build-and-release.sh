bc482980a5036426fc0f5339c4ed7072edc8e4e5
chirgwin <geoff@portchaw.com>
2016-09-25 15:01:46 -0700

    omit deprecated docker tag command `-f` switch

--- build-and-release.sh
+++ build-and-release.sh
@@ -3,5 +3,5 @@
 docker build -t portchaw/dind-node-rethink-build-runner . && \
   NODE_VER=$(docker run --rm --privileged portchaw/dind-node-rethink-build-runner node --version) && \
   IMAGE_TAG=${NODE_VER#"v"} && \
-  docker tag -f portchaw/dind-node-rethink-build-runner portchaw/dind-node-rethink-build-runner:$IMAGE_TAG && \
+  docker tag portchaw/dind-node-rethink-build-runner portchaw/dind-node-rethink-build-runner:$IMAGE_TAG && \
   docker push portchaw/dind-node-rethink-build-runner
